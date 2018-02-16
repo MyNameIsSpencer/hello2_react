889168aec1d4095a6e6b2b5ae66b1ac23864be58
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-02-15 21:17:39 -0500

    add tea time

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class Hello extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        <h1> Hey there Warld </h1>
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          It is tea time...
         </p>
+
       </div>
     );
   }
 }
 
-export default App;
+export default Hello;
