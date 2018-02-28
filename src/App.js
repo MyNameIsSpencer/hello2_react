7aff3c113aa2afb196cd198e9fb97449de21b8af
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-02-28 18:54:18 -0500

    add points paragraph

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import './App.css';
 class Hello extends Component {
   constructor(props) {
     super();
+    this.state = {
+      moodPoints:0
+    }
   }
 
   render() {
@@ -21,6 +24,13 @@ class Hello extends Component {
         <p> {this.props.name} is {this.props.age} old. He or she likes
          {this.props.favourites} </p>
         <p>{this.props.favourites[0]}, {this.props.favourites[1]}, {this.props.favourites[2]} and {this.props.favourites[3]}</p>
+        <p className="happiness"> On a 10 point scale, you are THIS HAPPY:</p>
+        <p className="happiness"> {this.state.moodPoints} </p>
+
+      <style>{`.happiness {
+               color: deepskyblue;
+               font-size: 25px;
+             } `}</style>
 
       </div>
     );
